8b0efebae1fb2123d10f59dcaa3369287ff35186
Lawrence Nno <lawrence.nno@gmail.com>
2023-07-21 16:24:18 +0100

    Task 3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,38 +1,25 @@
 #!/usr/bin/env python3
-""" Deletion-resilient hypermedia pagination """
+"""Deletion-resilient hypermedia pagination
+"""
 
 import csv
 import math
-from typing import List
+from typing import List, Dict, Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names. """
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
         self.__indexed_dataset = None
 
-    def dataset(self) -> List[List]:
-        """A cached dataset """
-        if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
 
-        return self.__dataset
-
-    def indexed_dataset(self) -> Dict[int, List]:
-        """A dataset indexed by sorting position, starts at zero """
-        if self.__indexed_dataset is None:
-            dataset = self.dataset()
-            truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
-        return self.__indexed_dataset
-
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        pass
+        return {
+            "index": index,
+            "next_index": i,
+            "page_size": page_size,
+            "data": d
+        }
