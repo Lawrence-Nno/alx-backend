32318b0a5e1b3080380845160a775e4e4d6fae3e
Lawrence Nno <lawrence.nno@gmail.com>
2023-07-21 16:12:58 +0100

    Task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -6,14 +6,14 @@ from typing import List, Tuple
 
 
 class Server:
-    """A server class to paginate a database of popular baby names. """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """ Cached dataset """
+        """Cached dataset """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -30,6 +30,29 @@ class Server:
         assert(page > 0 and page_size > 0)
         [start, end] = index_range(page, page_size)
         return self.dataset()[start: end]
+
+    def get_hyper(self, page: int, page_size: int) -> dict:
+        """
+        This funcstion returns a dictionary of:
+        page_size: Length of the returned dataset page
+        page: Current page number
+        data: Dataset page (equivalent to return from previous task)
+        next_page: Number of the next page or None if no next page
+        prev_page: Number of the previous page or None if no previous page
+        total_pages: Total number of pages in the dataset as an integer
+        """
+        dataset_records = self.get_page(page, page_size)
+
+        page_dict = math.ceil(len(self.__dataset) / page_size)
+
+        return {
+            'page_size': len(dataset_records),
+            'page': page,
+            'data': dataset_records,
+            'next_page': page + 1 if (page + 1) <= page_dict else None,
+            'prev_page': page - 1 if (page - 1) > 0 else None,
+            'total_pages': page_dict
+        }
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
